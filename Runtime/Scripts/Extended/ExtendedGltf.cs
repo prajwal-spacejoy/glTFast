9623ff1fbb384e9d309fc909b74503faf9be0d8b
Prajwal Shetty <prajwalshetty3000@gmail.com>
2021-03-31 20:32:22 +0530

    texture cdn construct issue fix

--- Runtime/Scripts/Extended/ExtendedGltf.cs
+++ Runtime/Scripts/Extended/ExtendedGltf.cs
@@ -9,12 +9,14 @@ public class ExtendedGltf
 {
     public static bool forceMipGeneration => LoadConfig.forceMipGeneration;
 
+    public string currentFileUniqueID; 
 
-    public static void AllocateTextureResourceFetchURI(Image img, Uri baseUri)
+
+    public void AllocateTextureResourceFetchURI(Image img, Uri baseUri)
     {
         if (string.IsNullOrEmpty(img.uri))
             return;
-        img.uri = LoadConfig.ConstructTextureURL(img.uri);
+        img.uri = LoadConfig.ConstructTextureURL(img.uri, currentFileUniqueID);
     }
 
 
