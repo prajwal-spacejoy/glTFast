46c95109f7f59a419a5f5ee6ae45e3290aa0eec4
Prajwal Shetty <prajwalshetty3000@gmail.com>
2021-04-01 13:12:00 +0530

    removed a unwanted log

--- Runtime/Scripts/GltFast.cs
+++ Runtime/Scripts/GltFast.cs
@@ -212,7 +212,6 @@ namespace GLTFast {
             )
         {
             this.downloadProvider = downloadProvider ?? new DefaultDownloadProvider();
-            Debug.LogError("Downloaded type: "+ this.downloadProvider.GetType().Name);
 
             if (deferAgent == null) {
                 if (defaultDeferAgent == null) {
