86bbeb043202057ef7320107d468cadbc40148fc
Prajwal Shetty <prajwalshetty3000@gmail.com>
2021-04-06 19:10:59 +0530

    dynamicly sets texture cdn

--- Runtime/Scripts/Extended/ExtendedGltf.cs
+++ Runtime/Scripts/Extended/ExtendedGltf.cs
@@ -1,27 +1,50 @@
-
-
 using GLTFast.Schema;
-using System;
 using System.IO;
-using UnityEngine;
 
 public class ExtendedGltf
 {
     public static bool forceMipGeneration => LoadConfig.forceMipGeneration;
 
-    public string currentFileUniqueID; 
+    public string currentFileUniqueID;
+    public string textureCdnURL;
 
-
-    public void AllocateTextureResourceFetchURI(Image img, Uri baseUri)
+    public ExtendedGltf(string currentFileUniqueID, string textureCdnURL)
     {
-        if (string.IsNullOrEmpty(img.uri))
-            return;
-        img.uri = LoadConfig.ConstructTextureURL(img.uri, currentFileUniqueID);
+        this.currentFileUniqueID = currentFileUniqueID;
+        this.textureCdnURL = textureCdnURL;
+    }
 
-        if (LoadConfig.GetTextureFetchMode() == TextureType.ktx)
+    public void AllocateTextureResourceFetchURI(Image img)
+    {
+        if (string.IsNullOrEmpty(img.uri) || !LoadConfig.enableTextureOverWeb)
+            return;
+
+        var textureFetchMode = LoadConfig.GetTextureFetchMode();
+        img.uri = ConstructTextureURL(img.uri, currentFileUniqueID, textureFetchMode);
+
+        if (textureFetchMode == TextureType.ktx)
             img.mimeType = LoadConfig.KtxMimeType;
     }
 
+
+    public string ConstructTextureURL(string filePath, string uniqueID, TextureType textureFetchMode)
+    {
+        string texUrl;
+        switch (textureFetchMode)
+        {
+            case TextureType.ktx:
+                texUrl = Path.GetFileNameWithoutExtension(filePath) + ".ktx2";
+                break;
+            default:
+                texUrl = Path.GetFileName(filePath);
+                break;
+        }
+
+        //Debug.Log("texture cdn: " + textureCDN.Append(uniqueID, textureFetchMode.ToString(), texUrl).AbsoluteUri);
+        //Debug.Log("texture cdn: " + string.Join("/", textureCdnURL, uniqueID, textureFetchMode.ToString(), texUrl));
+
+        return string.Join("/", textureCdnURL, uniqueID, textureFetchMode.ToString(), texUrl);
+    }
 
 
 }
