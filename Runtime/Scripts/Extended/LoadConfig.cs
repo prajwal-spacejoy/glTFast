f22167ee541c30f8210c83c3de9883a02e336d43
Prajwal Shetty <prajwalshetty3000@gmail.com>
2021-04-06 19:10:59 +0530

    dynamicly sets texture cdn

--- Runtime/Scripts/Extended/LoadConfig.cs
+++ Runtime/Scripts/Extended/LoadConfig.cs
@@ -1,46 +1,27 @@
 using System;
-using System.IO;
-using UnityEngine;
 
 namespace GLTFast.Schema
 {
 
-    [System.Serializable]
+    [Serializable]
     public static class LoadConfig
     {
         public const string KtxMimeType = "image/ktx2";
         public static bool forceMipGeneration;
+        public static bool enableTextureOverWeb;
 
-        static Uri textureCDN;
         static TextureType textureFetchMode = TextureType.raw;
-
-
-        public static string ConstructTextureURL(string filePath, string uniqueID)
-        {
-            string texUrl;
-            switch (textureFetchMode)
-            {
-                case TextureType.ktx:
-                    texUrl = Path.GetFileNameWithoutExtension(filePath) + ".ktx2";
-                    break;
-                default:
-                    texUrl = Path.GetFileName(filePath);
-                    break;
-            }
-            //Debug.Log("texture cdn: " + textureCDN.Append(uniqueID, textureFetchMode.ToString(), texUrl).AbsoluteUri);
-            return textureCDN.Append(uniqueID, textureFetchMode.ToString(), texUrl).AbsoluteUri;
-        }
 
         public static TextureType GetTextureFetchMode()
         {
             return textureFetchMode;
         }
 
-        public static void SetLoadMode(Uri textureCdn, TextureType textureFetchMode, bool forceMipGeneration)
+        public static void SetLoadMode(TextureType textureFetchMode, bool forceMipGeneration, bool enableTextureOverWeb)
         {
-            LoadConfig.textureCDN = textureCdn;
             LoadConfig.textureFetchMode = textureFetchMode;
             LoadConfig.forceMipGeneration = forceMipGeneration;
+            LoadConfig.enableTextureOverWeb = enableTextureOverWeb;
         }
 
     }
