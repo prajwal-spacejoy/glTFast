cea6d9e22483624a6f8e5978f3e8e0e1d6efbc6b
Praveen <praveen.sundar@spacejoy.com>
2022-07-22 20:00:16 +0530

    updated support for ktx textures

--- Runtime/Scripts/GltfImport.cs
+++ Runtime/Scripts/GltfImport.cs
@@ -711,6 +711,15 @@ namespace GLTFast {
             return null;
         }
         
+        // Added by Spacejoy
+        // Returns list of images to check the image mime type for ktx textures
+        public Image[] GetAllSourceImages()
+        {
+            if (gltfRoot?.images != null)
+                return gltfRoot.images;
+            return null;
+        }
+        
         /// <inheritdoc />
         public Matrix4x4[] GetBindPoses(int skinId)
         {
@@ -820,8 +829,11 @@ namespace GLTFast {
                 var destSplitArray = new string[urlSplit.Length - 2];
                 Array.Copy(urlSplit, 0, destSplitArray, 0, urlSplit.Length - 2);
                 var textureCDNCore = string.Join("/", destSplitArray);
-
-                ExtendedGltf gltfExt = new ExtendedGltf(System.IO.Path.GetFileNameWithoutExtension(baseUri.LocalPath), textureCDNCore);
+                
+                // Changed by Spacejoy
+                // base uri removes the filename with extension at the end. ExtendedGltf constructor expects asset id as first parameter
+                var fileName_NoExtension = urlSplit[urlSplit.Length - 2];
+                ExtendedGltf gltfExt = new ExtendedGltf(fileName_NoExtension, textureCDNCore);
                 foreach (var img in gltfRoot.images)
                 {
                     gltfExt.AllocateTextureResourceFetchURI(img);
